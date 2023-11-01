7e9e10b25168842bb103b7f7219e8f00575bdec7
Mehmet Enes <mehenar@gmail.com>
2023-11-02 00:06:00 +0300

    OPERATÖRLER

--- src/DERSLER/A5_Class.java
+++ src/DERSLER/A5_Class.java
@@ -3,5 +3,24 @@ package DERSLER;
 public class A5_Class {
     public static void main(String[] args) {
 
+        int a = 10;
+        System.out.println("bir :"  + ++a);
+      //  System.out.println("iki : " + a++);
+        System.out.println("üç :" + a);
+
+        System.out.println("-------------------------------------------------------------------");
+        //concatination (String Birleştirme
+
+        String w = "Hello";
+        String e = "World";
+        int r = 5;
+        System.out.println(w+e); //HelloWorld
+        System.out.println(w + " " + e); //Hello World
+        System.out.println(w+r+e);//Hello5World
+        System.out.println(r+w+e);//5HelloWorld
+
+        //Karşılaştırma operatörü == < > ! || &&
+
+
     }
 }
