48aad519867420244f67c4d9cb8948779e426196
Mehmet Enes <mehenar@gmail.com>
2023-11-02 19:15:44 +0300

    swştch

--- src/DERSLER/A8_SwitchCase.java
+++ src/DERSLER/A8_SwitchCase.java
@@ -1,8 +1,59 @@
 package DERSLER;
 
+import java.util.Scanner;
+
 public class A8_SwitchCase {
     public static void main(String[] args) {
         //Kullanıcıdan ay numarasını alıp ay ismini yazdırın
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayi olarak ay numarasını yazınız");
+        int ayNo = scan.nextInt();
+
+        switch (ayNo){
+            case 1 :
+                System.out.println("Ocak");
+                break;
+            case 2:
+                System.out.println("Şubat");
+                break;
+            case 3 :
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Nisan");
+                break;
+            case 5 :
+                System.out.println("Mayıs");
+                break;
+            case 6:
+                System.out.println("Haziran");
+                break;
+            case 7 :
+                System.out.println("Temmuz");
+                break;
+            case 8:
+                System.out.println("Ağustos");
+                break;
+            case 9 :
+                System.out.println("Eylül");
+                break;
+            case 10:
+                System.out.println("Ekim");
+                break;
+            case 11 :
+                System.out.println("Kasım");
+                break;
+            case 12:
+                System.out.println("Aralık");
+                break;
+
+            default:
+                System.out.println("Yanlış değer girdiğiniz 1 ile 12 arasında değer giriniz");
+
+
+
+        }
 
 
     }
