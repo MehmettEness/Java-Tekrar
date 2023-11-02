2392801c4b6fd3929dcb20a7f15b575e8ed4197e
Mehmet Enes <mehenar@gmail.com>
2023-11-02 19:08:18 +0300

    swştch

--- src/DERSLER/A08_SwitchCase.java
+++
@@ -1,7 +0,0 @@
-package DERSLER;
-
-public class A08_SwitchCase {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/DERSLER/A8_SwitchCase.java
@@ -0,0 +1,9 @@
+package DERSLER;
+
+public class A8_SwitchCase {
+    public static void main(String[] args) {
+        //Kullanıcıdan ay numarasını alıp ay ismini yazdırın
+
+
+    }
+}
